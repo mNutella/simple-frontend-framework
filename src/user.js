81d7934537d1ba3cc4bb48a241ae3d87228a84d2
nutella <nutell@4ka.com>
2022-01-25 14:15:42 -0500

    feat: implemented component state

--- src/user.js
+++ src/user.js
@@ -1,5 +1,11 @@
 import { div } from "../framework/element";
 import { onClick } from "../framework/event";
 
+const initialState = { firstName: "XXX", lastName: "YYY" };
+
+const methods = {
+  changeName: (state, firstName) => ({ ...state, firstName })
+};
+
 export const User = ({ firstName, lastName }) => 
   div`${onClick(() => alert(firstName))} Hello ${firstName} ${lastName}!`;
